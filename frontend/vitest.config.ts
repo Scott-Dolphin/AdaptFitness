b971c193555e664605278c42073bce2ad9d0b94c
Scott Dolphin <nathanjj1225@gmail.com>
2026-02-12 23:07:45 -0800

    added support for netlify deployment

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
+import tsconfigpPaths from "vite-tsconfig-paths";
+import netlifyReactRouter from "@netlify/vite-plugin-react-router";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    netlifyReactRouter()
+    // netlify()
+
+  ],
   test: {
     globals: true,
     environment: 'happy-dom',
